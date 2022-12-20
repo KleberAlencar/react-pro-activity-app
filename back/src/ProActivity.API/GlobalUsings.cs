75910c4042e43059064251b372eda4f6bda9aae9
KLEBER_ALENCAR <kleber.alencar@outlook.com>
2022-12-20 15:03:41 -0300

    .NET 5 to .NET 6

---
+++ back/src/ProActivity.API/GlobalUsings.cs
@@ -0,0 +1,12 @@
+global using Microsoft.OpenApi.Models;
+global using ProActivity.Data.Context;
+global using Microsoft.AspNetCore.Builder;
+global using Microsoft.EntityFrameworkCore;
+global using ProActivity.Domain.Services;
+global using Microsoft.Extensions.Hosting;
+global using System.Text.Json.Serialization;
+global using ProActivity.Data.Repositories;
+global using Microsoft.Extensions.Configuration;
+global using ProActivity.Domain.Interfaces.Services;
+global using Microsoft.Extensions.DependencyInjection;
+global using ProActivity.Domain.Interfaces.Repositories;
